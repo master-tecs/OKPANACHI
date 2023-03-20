564ea42320d6909a0b2249ecbc4cf2d368c1e847
master-tecs <masterawahab@gmail.com>
2023-03-20 18:58:06 +0300

    Created and added viewService component.

--- components/Service/index.jsx
+++ components/Service/index.jsx
@@ -1,8 +1,16 @@
-import { ArrowForwardRounded } from "@mui/icons-material";
+import { ArrowForwardRounded, Close } from "@mui/icons-material";
 import { motion } from "framer-motion";
+import { useState } from "react";
 import styles from "../../styles/Service.module.css";
 
 function Service({ Icon, title, details }) {
+  const [view, setView] = useState(false);
+  const maxLength = 210;
+
+  const handleView = () => {
+    setView(!view);
+  };
+
   return (
     <motion.div
       className={styles.service}
@@ -32,7 +40,7 @@ function Service({ Icon, title, details }) {
             transition={{ delay: 0.15, type: "tween", duration: 0.15 }}
             initial={{ opacity: 0, y: 100 }}
           >
-            {details}
+            {details.substring(0, maxLength) + "..."}
           </motion.p>
         </div>
         <motion.span
@@ -40,13 +48,79 @@ function Service({ Icon, title, details }) {
           whileInView={{ opacity: 1 }}
           transition={{ delay: 0.25, type: "tween", duration: 0.15 }}
           initial={{ opacity: 0 }}
+          onClick={() => handleView()}
         >
           <p>Tap for more</p>
           <ArrowForwardRounded />
         </motion.span>
       </div>
+      {view && (
+        <ViewService
+          Icon={Icon}
+          title={title}
+          details={details}
+          handleView={handleView}
+        />
+      )}
     </motion.div>
   );
 }
 
 export default Service;
+
+//
+// ------------------------
+// VIEW SERVICE  COMPONENT
+// ------------------------
+//
+
+function ViewService({ Icon, title, details, handleView }) {
+  return (
+    <div className={styles.viewService}>
+      <div className={styles.modalOverlay} onClick={() => handleView()}></div>
+
+      <div className={styles.container}>
+        <div className={styles.info}>
+          <motion.div
+            className={styles.icon}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ type: "tween", duration: 0.15 }}
+            initial={{ opacity: 0, y: 100 }}
+          >
+            <Icon />
+          </motion.div>
+          <motion.h4
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.1, type: "tween", duration: 0.15 }}
+            initial={{ opacity: 0, y: 100 }}
+          >
+            {title}
+          </motion.h4>
+          <motion.p
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.15, type: "tween", duration: 0.15 }}
+            initial={{ opacity: 0, y: 100 }}
+          >
+            {details}
+          </motion.p>
+        </div>
+
+        <div className={styles.control}>
+          <motion.span
+            className={styles.cta}
+            whileInView={{ opacity: 1 }}
+            transition={{ delay: 0.25, type: "tween", duration: 0.15 }}
+            initial={{ opacity: 0 }}
+            onClick={() => handleView()}
+          >
+            <p>Tap to close</p>
+            <Close />
+          </motion.span>
+          {/* <span onClick={() => handleView()}>
+            <Close className="modalCloseIcon" />
+          </span> */}
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Services/index.jsx
+++ components/Services/index.jsx
@@ -24,48 +24,192 @@ function Services() {
           <Service
             Icon={AdUnits}
             title="Audit and Accounting"
-            details="After completing the various steps, set out above we would then be in a       position to draft out audit report on the accounts."
+            details="We adopt a systematic approach to our audit engagements. This requires us to study the clients system of internal control and to evaluate it in order to identify the features that are important to the audit. This evaluation enables us to determine the controls on which we propose to rely and to take this into account in deciding our programme of audit tests.
+ 
+            Main Elements of the Firm’s Audit Approach 
+            The single most important element is applying the firm’s audit approach on the exercise of informed judgments at various stages of the audit. This requires that important decisions are made by the senior people on the audit i.e. the engagement partner and manager, based on their understanding of the client’s industry, business, organization and accounting system.
+             
+            The firm’s audit approach is directed to the expression of an opinion on the accounts and is summarized below. We would:
+             
+            (a) Familiarize our self with the clients business and organization including those  	organizational arrangements and control procedures (financial controls) that:
+             
+            (i) Provide conditions under which the system of accounting and internal controls can operate efficiently; and
+            (ii) In part, monitor performance of the accounting system.
+             
+            (b) Obtain an understanding and record of the client’s accounting system, including the procedures and controls incorporated in it and review selected transactions to confirm that we have understood and recorded the system properly.
+             
+            (c) Evaluate the internal control with the aid of internal control questionnaires (ICQs) to identify:           	
+            (i)Those internal accounting controls upon which we propose to place reliance;  and
+            (ii) Any apparent weakness in the system.
+                             Internal Accounting Controls Comprise:
+            (i)                 Basic controls, i.e. those controls designed to ensure that transactions are recorded   completely and accurately. Basic controls include such features as control accounts and numerical controls to ensure that all sequentially numbered documents are accounted for.
+             
+            (ii)               Discipline over basic controls, i.e. those features of a system that are designed to ensure that the basic controls continue to operate properly and that the assets are safeguarded. Discipline over basic controls comprise divisions of duties, separation of the custody of assets from the accounting responsibility for them, physical arrangement that prevent unauthorised access to assets or accounting records, and supervision of the result of one persons work by another. The effectiveness of the discipline over basic controls will determine our ability to rely on the continuity of operation of the basic controls and the extent to which it is necessary to carry out tests of the basic controls themselves.
+             
+            (d) Discussions with the clients the apparent weaknesses identified in the system, i.e. weaknesses shown by the answers to the questions relating to each control objective.
+             
+            (e) Carry out compliance tests.
+                It is the firms policy that we should carry out a programme  of compliance tests on those internal accounting controls on which we plan to rely to ensure the continued and proper operation of the accounting system.  
+             
+            Compliance tests are designed to provide evidence that the internal accounting controls as identified in the ICQ are being operated as planned. Compliance tests involve examination of evidence (records, documents, reconciliation, reports for evidence that a specific control appears to have been properly applied) and reperformance (repeating either in whole or part of the same procedure performed by the clients employees.
+             
+            When carrying out these tests we should review the nature and reasonableness of the transaction recorded, observe generally the circumstances in which the operations are being carried out, and be alert to any thing that seems to be out of the ordinary.
+             
+            The following categories of control will be tested:
+            -          Basic controls
+            -          Segregation of  duties
+            -          Supervisory controls
+            -          Physical controls
+            -          Integrity controls (in  computer system)
+             
+             
+            (f) Management letters
+            After we have completed the evaluation of internal control and the compliance tests, we could report to the client or internal control deficiencies that have come to our attention. If additional matters come to our attention as a result of substantive tests, these would be reported to the client.
+             
+            (g) Substantive tests.
+            Substantive tests represent the final stage of the audit leading to the expression of an audit opinion. They comprise direct tests of account balances and other information contained in the account and certain other procedures/tests of a more general nature that indirectly provide evidence of the validity of the amount appearing in the accounts.
+            Substantive tests are generally directed towards balance sheet and profit and loss (Income and Expenditure) account items.
+             
+            (h)  Audit Opinion
+            After completing the various steps, set out above we would then be in a       position to draft out audit report on the accounts.   If difficulties become apparent or if there are matters, which make it difficult for us to give a clean audit report, we would immediately discuss these with our clients to see whether some alternative or additional information can be produced to solve the problem.
+             
+            The objective of this approach is to perform an effective audit at lowest cost,  	taking maximum account of the internal accounting procedures set up by the client to ensure the completeness and accuracy of the processing of accounting information and the custody of assets.
+            "
           />
           <Service
             Icon={AccountBalanceRounded}
             title="Accounting
 "
-            details="The statutory legislation setting up these organization require that proper books of accounts be maintained and annual audited accounts submitted to the appropriate authorities (Registrar of companies, Director General, Supervising Ministry) on a yearly basis."
+            details="The statutory legislation setting up these organization require that proper books of accounts be maintained and annual audited accounts submitted to the appropriate authorities (Registrar of companies, Director General, Supervising Ministry) on a yearly basis.
+ 
+            For one reason or the other some organizations are unable to hire services of full time professional accountants to institutes a proper accounting system. And as such their books of accounts are not written up on a regular basis and in some cases are years in arrears.
+             
+            As the organization grow and become more complex, staff may take advantage of such situations to the detriment of that organization.
+             
+            As professional accountants we undertake to:
+            a)   Maintain books of accounts for organizations.
+            b)   Implement accounts manuals.    	
+            c)   Plan and install computer - based accounting system.
+             
+            "
           />
           <Service
             Icon={MoneyOffCsredRounded}
             title="Taxation"
-            details="Complexities in Nigeria tax legislation have increased tremendously in recent times with new taxes being generated. The methods of arriving at the quantum of tax payable keep changing and complicated."
+            details="Complexities in Nigeria tax legislation have increased tremendously in recent times with new taxes being generated. The methods of arriving at the quantum of tax payable keep changing and complicated.
+	
+            We as experts in this field will help to over come these problems.
+             
+            In respect of each accounting period, we prepare a computation of profits adjusted in accordance with the provisions of the tax laws, for the purpose assessment to companies income tax. This then submitted to the Federal Inland Revenue services as being the company’s formal return and then proceeds to agree the company’s tax liability.
+             
+            We normally object against excessive or incorrect assessments to companies’ income tax where notice is received by us.
+             
+            We advise companies generally on Tax matters as they arise. We also offer advice as to the taxation implications of a particular aspect of the companies’ business transactions and of a particular aspect of the companies’ affairs – such as expansion projects, business combinations and capital reconstruction.
+            We are also involved in Accelerated Revenue Generation for some State Governments and Local Government Councils.  Our functions also include VAT monitoring, and collection taxes on behalf of the Education Tax Fund.
+             
+            We normally carry out this work in accordance with the best current practice.
+             
+            "
           />
           <Service
             Icon={CommentBank}
             title="Management consultancy"
-            details="Many enterprises in the economy whether they belong to the public or private  sector have been inflicted with many ills due to the prevailing harsh economic conditions."
+            details="Many enterprises in the economy whether they belong to the public or private  sector have been inflicted with many ills due to the prevailing harsh economic conditions. Some of these ills which managers of these enterprises face include lack of raw materials, loss of market capacity under utilization etc.
+ 
+            Not all of these enterprises survive, only those that maintain corporate brilliance manned by creative and innovation managers who are well equipped by the new management systems and skills, stay in business.
+             
+            In financial difficulties, we offer support services to enterprises in difficulties. This we do by analyzing problems, identifying opportunities, and recommending practical solutions, which can be subsequently implemented under our supervision to achieve the desired results.
+             
+            In addition to the above function we offer the following services:
+             
+            a)     	Development of systems and procedures: Our approach, include the review of the existing system, identifying the strengths and the weaknesses of the system, development of new systems and implementing same
+             
+            b)    	We also undertake staff recruitment and training.
+             
+            c)     	Writing up an update of Fixed Assets Register.
+             
+            d)    	Market and operational studies and implementation.
+             
+            e)     	Preparation of feasibility studies.
+             
+            f)     	Design  Administrative manual.
+             
+            g)     	Management and efficiency audits.
+             
+            h)    	Corporate Reconstruction and Planning etc.
+             
+            "
           />
           <Service
             Icon={InsertChartOutlined}
             title="Insolvency"
-            details="Nigeria has been facing economic recession in recent times, which has place a number of Nigerian enterprises in economic coma and where they survive they merely exists."
+            details="Nigeria has been facing economic recession in recent times, which has place a number of Nigerian enterprises in economic coma and where they survive they merely exists.
+ 
+            This has posed serious problems, Banks and Government. Lenders of permanent capital are reluctant to extend further credits to these enterprises in order not to increase their exposure. How they (the lenders) are to recover their initial credits becomes their initial pre-occupation.
+             
+            Provisions are made in the conditions of offer of the initial credit, in case the borrower is unable to meet the repayment terms, for the appointment of either to receiver manager or liquidator.
+             
+            Lenders of prepayment capital are therefore faced with the problems of either to rescue the enterprises from financial problems themselves or appoint receiver manager or liquidator.
+             
+            Our firm provides these services to Lender of permanent capital of troubled enterprises with the view of rescuing them out of their financial doldrums.
+             
+            "
           />
           <Service
             Icon={LayersTwoTone}
             title="Secretarial"
-            details="Procedures for incorporation of companies are provided in the companies and Allied Matters Decree 1990..."
+            details="Procedures for incorporation of companies are provided in the companies and Allied Matters Decree 1990. Once a company has been incorporated there are statutory returns that are required to be filed, and statutory books required to be kept at its registered office. Minutes of meetings prescribed in the Decree have to be recorded and preserved.
+ 
+            Our firm handles:
+            a)     	Incorporation of Companies
+             
+            b)    	Filing the statutory returns
+             
+            c)     	Keeping of statutory books/registers
+             
+            d)    	Records and preserves minutes of meetings and
+             
+            e)     	Ensures that the companies comply generally with the provision of the Companies and Allied Matters Decree 1990.
+             
+            f)     	Preparation and processing of exchange control and business permit applications.
+             
+"
           />
           <Service
             Icon={InfoSharp}
             title="Information communication technology (ICT) Services"
             details="ICT services include:
-                    Networking,
-                    Solutions Packaging,
-                    Multi – vendor/multi – platform system integration,
-                    Trends Tracking and Research...
+ 
+            Networking
+           Solutions Packaging
+           Multi – vendor/multi – platform system integration
+            Trends Tracking and Research
+           Design, Development and Installation of Computerized Accounting system.
+            Computer Audit (using CAAT- ACL)
+            The following components singly or jointly.
+            Payroll personal system
+            Sales/Accounts Receivable system
+            Purchases/Accounts payable system
+            Stores management system
+           Computer based Asset Register.
+           
                     "
           />
           <Service
             Icon={PeopleAltOutlined}
             title="Staff  Training. And Development"
-            details="Human resources are very vital to the success of an organization. Anything that could be done to enhance the performance and therefore productivity should be embarked upon."
+            details="Human resources are very vital to the success of an organization. Anything that could be done to enhance the performance and therefore productivity should be embarked upon. There is a continuing requirement in organizations to develop their personnel for higher level of performance and responsibilities. Organizations that have accepted this challenge and acceded to the training needs of their personnel have been successful.
+ 
+            We have realized that employees without adequate training lack confidence in the exemption of their assigned responsibilities and this may adversely affect productivity. An employee should be helped to grow into accepting more responsibilities by systematic training and re-training.
+             
+            We design special training programmes for different levels of staff taking into account their different peculiarities required for their assignments.
+             
+            Our training programmes offer the following benefits to the organisations.
+             Increased productivity
+              Increased employee morale
+            Reduction in supervision time
+            Industrial harmony
+            "
           />
         </div>
       </div>
